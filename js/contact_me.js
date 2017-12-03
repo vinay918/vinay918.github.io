1cfb4dffc440a89b72501a6ae8adada8239643ef
vinay918 <vinaysingh@skiptuff.com>
2017-12-02 21:56:04 -0600

    Test

--- js/contact_me.js
+++ js/contact_me.js
@@ -41,16 +41,16 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        //error: function() {
+        error: function() {
           // Fail message
-        //  $('#success').html("<div class='alert alert-danger'>");
-         // $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-         //   .append("</button>");
-         // $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", the website is currently hosted on Github which doesn't support php scripts. Please email me at vbaldevsingh@cse.unl.edu instead!"));
-         // $('#success > .alert-danger').append('</div>');
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
           //clear all fields
-       //   $('#contactForm').trigger("reset");
-       // },
+          $('#contactForm').trigger("reset");
+        },
         complete: function() {
           setTimeout(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
