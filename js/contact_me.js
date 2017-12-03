d28925016c8558f773d593269996d39f42edcaf8
vinay918 <vinaysingh@skiptuff.com>
2017-12-02 21:09:32 -0600

    Changed Contact information

--- js/contact_me.js
+++ js/contact_me.js
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", the website is currently hosted on Github which doesn't support php scripts. Please leave me an email instead!!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", the website is currently hosted on Github which doesn't support php scripts. Please email me at vbaldevsingh@cse.unl.edu instead!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
